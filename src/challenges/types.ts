1135f210627da9120e5c0d018121295f2e140cce
Jake 'Sid' Smith <hello@jakesidsmith.com>
2022-09-25 15:33:46 +0100

    Add updated types

--- src/challenges/types.ts
+++ src/challenges/types.ts
@@ -1,12 +1,33 @@
-export type PrimativeValue = string | number | object;
-export type Primative = PrimativeValue | PrimativeValue[];
-export type Rule = 'no-sort' | 'no-add' | 'no-eval';
+export type PrimitiveValue =
+  | string
+  | number
+  | object
+  | boolean
+  | null
+  | undefined;
+export type Primitive = PrimitiveValue | readonly PrimitiveValue[];
 
-export interface Challenge {
+export interface Example<A extends readonly Primitive[], R extends Primitive> {
+  input: A;
+  output: R;
+}
+
+export interface Assertion<
+  A extends readonly Primitive[],
+  R extends Primitive
+> {
+  input: A;
+  output: R | ((play: (...args: A) => R, input: A) => R);
+}
+
+export interface Challenge<
+  A extends readonly Primitive[],
+  R extends Primitive
+> {
   title: string;
-  input: Primative;
-  output: Primative;
   description: string;
-  example: string;
-  rules?: Rule[];
+  example: Example<A, R>;
+  assertions: readonly Assertion<A, R>[];
+  assertRules?: (playString: string) => void;
+  context?: Record<string, any>;
 }
